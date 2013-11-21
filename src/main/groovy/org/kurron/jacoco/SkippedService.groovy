da9aea2f47c6af50293f4566a9b839726ea800de
Ron Kurr <rkurr@transparent.com>
2013-11-21 15:58:44 -0500

    Finally got the exlusion stuff to work.

---
+++ src/main/groovy/org/kurron/jacoco/SkippedService.groovy
@@ -0,0 +1,11 @@
+package org.kurron.jacoco
+
+/**
+ * Dummy service needed to test drive the code coverage tool.
+ */
+class SkippedService {
+
+    String echo( String message ) {
+        "Just heard ${message}"
+    }
+}
